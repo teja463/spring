9cb1225d7a38c7b25e1cc24a9af2c52d918ed0bd
Teja P <teja463@gmail.com>
2016-12-29 15:58:46 +0530

    Added test cases

---
+++ ioc/src/test/java/com/teja/spring/ioc/FirstMainTest.java
@@ -0,0 +1,15 @@
+package com.teja.spring.ioc;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class FirstMainTest extends TestCase {
+
+	public void testMain() {
+		Assert.assertTrue(true);
+	}
+
+	public void test2(){
+		Assert.assertEquals(true, true);
+	}
+}
