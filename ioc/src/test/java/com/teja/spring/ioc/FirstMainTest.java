33482328e5eeb65cea57527cd18d38577a4c79b2
Teja P <teja463@gmail.com>
2017-01-03 11:53:15 +0530

    added logger statements

--- ioc/src/test/java/com/teja/spring/ioc/FirstMainTest.java
+++ ioc/src/test/java/com/teja/spring/ioc/FirstMainTest.java
@@ -1,11 +1,17 @@
 package com.teja.spring.ioc;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class FirstMainTest extends TestCase {
 
+	private static final Log logger = LogFactory.getLog(FirstMainTest.class);
+	
 	public void testMain() {
+		logger.debug("main test case..");
 		Assert.assertTrue(true);
 	}
 
