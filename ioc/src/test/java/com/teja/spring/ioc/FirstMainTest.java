7df62c17693acfa74f46a57a2a376ac761bce5cf
Teja P <teja463@gmail.com>
2017-01-05 12:13:16 +0530

    Using annotations now.

--- ioc/src/test/java/com/teja/spring/ioc/FirstMainTest.java
+++ ioc/src/test/java/com/teja/spring/ioc/FirstMainTest.java
@@ -2,19 +2,21 @@ package com.teja.spring.ioc;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
+import org.junit.Test;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
 
-public class FirstMainTest extends TestCase {
+public class FirstMainTest {
 
 	private static final Log logger = LogFactory.getLog(FirstMainTest.class);
 	
+	@Test
 	public void testMain() {
 		logger.debug("main test case..");
 		Assert.assertTrue(true);
 	}
 
+	@Test
 	public void test2(){
 		Assert.assertEquals(true, true);
 	}
