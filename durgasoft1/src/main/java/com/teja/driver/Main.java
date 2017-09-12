f9f0927c65743170f8b0c36a9c3fa3f2b3f1dec4
teja463 <teja463@gmail.com>
2017-09-12 19:47:26 +0530

    Added two approches for IoC

--- durgasoft1/src/main/java/com/teja/driver/Main.java
+++ durgasoft1/src/main/java/com/teja/driver/Main.java
@@ -2,6 +2,8 @@ package com.teja.driver;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -10,8 +12,15 @@ import com.teja.beans.Bean1;
 public class Main {
 
 	public static void main(String[] args) {
-		Resource resource = new ClassPathResource("resources/spring.xml");
-		BeanFactory factory = new XmlBeanFactory(resource);
+		
+		
+		/** Approach1 using BeanFactory */
+		/*Resource resource = new ClassPathResource("resources/spring.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);*/
+		
+		/** Approach 2 using Application Context */
+		ApplicationContext factory = new ClassPathXmlApplicationContext("resources/spring.xml");
+		
 		
 		Bean1 bean1 = (Bean1)factory.getBean("bean1");
 		bean1.sayHello();
