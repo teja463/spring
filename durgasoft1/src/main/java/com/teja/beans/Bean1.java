e5897582d818336c6849d03e9513e34db7cd6338
jraghu <Jishnu.Raghu@aig.com>
2017-09-12 18:50:39 +0530

    Added durgasoft demo project

---
+++ durgasoft1/src/main/java/com/teja/beans/Bean1.java
@@ -0,0 +1,8 @@
+package com.teja.beans;
+
+public class Bean1 {
+
+	public void sayHello(){
+		System.out.println("Hello!!!");
+	}
+}
---
+++ durgasoft1/src/main/java/com/teja/driver/Main.java
@@ -0,0 +1,19 @@
+package com.teja.driver;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.teja.beans.Bean1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Resource resource = new ClassPathResource("resources/spring.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+		Bean1 bean1 = (Bean1)factory.getBean("bean1");
+		bean1.sayHello();
+		
+	}
+}
