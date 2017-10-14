c9fa37f98ef8c4efca2f397a9116598987693e18
teja463 <teja463@gmail.com>
2017-10-14 23:31:20 +0530

    Now it is similar to UploadServlet1

--- test/src/com/teja/sample/UploadServlet2.java
+++ test/src/com/teja/sample/UploadServlet2.java
@@ -6,16 +6,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.teja.sample.listener.MyAsyncListener;
+
 /**
  * Servlet implementation class UploadServlet2
  */
-@WebServlet("/UploadServlet2")
+@WebServlet(urlPatterns="/UploadServlet2")
+@MultipartConfig
 public class UploadServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -31,20 +36,33 @@ public class UploadServlet2 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		InputStream in = request.getPart("file").getInputStream();
-		InputStreamReader isr = new InputStreamReader(in);
-		BufferedReader br = new BufferedReader(isr);
-		PrintWriter pw = response.getWriter();
-		String line = br.readLine();
-		StringBuilder sb = new StringBuilder();
-		while(line!=null){
-			sb.append(line+"\n");
-			line=br.readLine();
-		}
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Long start = System.currentTimeMillis();
+		processFile(request, response);
+		Long end = System.currentTimeMillis();
+		System.out.println("Total time: "+(end-start));
 	}
 
+	private void processFile(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
+		InputStream in = request.getPart("file").getInputStream();
+		BufferedReader br = new BufferedReader( new InputStreamReader(in));
+		PrintWriter pw = response.getWriter();
+		String line = br.readLine();
+		while(line!=null){
+			pw.write(line+"\n");
+			line=br.readLine();
+//			response.flushBuffer();
+			pw.flush();
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+//		pw.close();
+	}
+
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
