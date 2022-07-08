103b10a0dcb13e30860e614c95f3fb695c294d80
Brahma Teja Ponnuru <brahma.teja.ponnuru@collins.com>
2022-07-08 11:16:26 +0530

    added open api doc and changed ports

--- springboot-unit-testing/src/main/java/com/example/springbootunittesting/StudentManagementController.java
+++ springboot-unit-testing/src/main/java/com/example/springbootunittesting/StudentManagementController.java
@@ -2,10 +2,13 @@ package com.example.springbootunittesting;
 
 import com.example.springbootunittesting.springdeveloper.Reservation;
 import com.example.springbootunittesting.springdeveloper.ReservationRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name="student-controller", description = "Controller to update student Info")
 @RestController
 @RequestMapping("/mgmt/students")
 public class StudentManagementController {
@@ -16,25 +19,29 @@ public class StudentManagementController {
         this.reservationRepository = reservationRepository;
     }
 
+
+    @Operation(summary = "foo", description = "bar")
     @GetMapping
     public List<Reservation> getAll(){
         return reservationRepository.findAll();
     }
 
     @PostMapping
-    public void addUser(){
+    public Reservation addUser(Reservation input){
         System.out.println("User added");
+        return new Reservation();
     }
 
 
     @PutMapping
-    public void updateUser(){
+    public Reservation updateUser(Reservation res){
         System.out.println("User Updated");
+        return new Reservation();
     }
 
 
-    @DeleteMapping
-    public void deleteUser(){
+    @DeleteMapping("/id/{id}")
+    public void deleteUser(int id){
         System.out.println("User Deleted");
     }
 }
