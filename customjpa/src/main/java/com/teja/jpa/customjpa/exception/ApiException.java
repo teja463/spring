71cd23aff6bfc33aab9dcd75257e88f16e04bfac
Brahma Teja Ponnuru <brahma.teja.ponnuru@collins.com>
2023-06-13 10:38:14 +0530

    removed unused files

--- customjpa/src/main/java/com/teja/jpa/customjpa/exception/ApiException.java
+++ customjpa/src/main/java/com/teja/jpa/customjpa/exception/ApiException.java
@@ -1,10 +1,6 @@
 package com.teja.jpa.customjpa.exception;
 
-import com.teja.jpa.customjpa.advice.ApiExceptionAdvice;
-
 public class ApiException extends RuntimeException{
-
-
 
     public ApiException(){
 
--- customjpa/src/main/java/java8/Greeting.java
+++
@@ -1,6 +0,0 @@
-package java8;
-
-public interface Greeting {
-
-    void perform();
-}
